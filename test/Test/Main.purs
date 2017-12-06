2b641d4acfd0cc12ec716b196a0a6c8cb422b2d4
Forest Loren Toney III <foresttoney@gmail.com>
2017-12-05 17:07:31 -0800

    Fix warnings

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,9 +1,15 @@
 module Test.Main where
 
 import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Random (RANDOM)
 import Test.Data.Binary.Base64 as Binary
 import Test.Data.String.Base64 as String
 
+main :: Eff (console :: CONSOLE, random :: RANDOM, exception :: EXCEPTION) Unit
 main = do
   Binary.testBase64
   String.testBase64
